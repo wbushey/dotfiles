3d1b64c145b12ab03c7ac1c5b66751fbbb6a89fe
Bill Bushey <wbushey@gmail.com>
2018-07-27 18:21:45 -0500

    NVM auto switching

--- .shell/node.sh
+++ .shell/node.sh
@@ -1,3 +1,52 @@
 export PATH="node_modules/.bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm_is_installed(){
+  [ -s "$NVM_DIR/nvm.sh" ]
+}
+
+if nvm_is_installed; then
+  . "$NVM_DIR/nvm.sh" --no-use
+
+  # Thanks to https://github.com/jsdf/nvm_auto/blob/master/nvm_auto.sh
+  unset NVM_AUTO_VERSION
+
+  function nvm_auto() {
+    local dir="$PWD/" version
+
+    until [[ -z "$dir" ]]; do
+      dir="${dir%/*}"
+
+      if { read -r version <"$dir/.nvmrc"; } 2>/dev/null || [[ -n "$version" ]]; then
+        version="${version%%[[:space:]]}"
+
+        if [[ "$version" == "$NVM_AUTO_VERSION" ]]; then return
+        else
+          NVM_AUTO_VERSION="$version"
+
+          nvm which "$version" > /dev/null 2>&1
+          if [[ $? -ne 0 ]]; then
+            echo "nvm_auto: version $version not installed. Run 'nvm install' to install it"
+            return $?
+          fi
+
+          nvm use "$version"
+          return $?
+        fi
+      fi
+    done
+
+    if [[ -z "$NVM_AUTO_NO_RESET" && -n "$NVM_AUTO_VERSION" ]]; then
+      nvm use default
+      unset NVM_AUTO_VERSION
+    fi
+  }
+
+  if [[ -n "$ZSH_VERSION" ]]; then
+    if [[ ! "$chpwd_functions" == *nvm_auto* ]]; then
+      chpwd_functions+=("nvm_auto")
+    fi
+  elif [[ -n "$BASH_VERSION" ]]; then
+    trap '[[ "$BASH_COMMAND" != "$PROMPT_COMMAND" ]] && nvm_auto' DEBUG
+  fi
+fi
