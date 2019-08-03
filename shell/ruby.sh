0bddb24a0c14ddf2146ddaabf3fcd0bc5c72ce28
Bill Bushey <wbushey@gmail.com>
2019-08-03 08:35:17 -0500

    Reenabling bi command

--- shell/ruby.sh
+++ shell/ruby.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-export RBENV_ROOT=$HOME/.dotfiles/.ruby/rbenv
-export PATH="$RBENV_ROOT/bin:$PATH"
-eval "$(rbenv init -)"
-
 # Make bundled executables a bit more natural to use
 alias bi='bundle install --binstubs .bundle/bin --path vendor/bundle'
 export PATH=".bundle/bin:$PATH"
